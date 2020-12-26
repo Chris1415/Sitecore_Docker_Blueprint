a454c466035fd62e10eb4a07af26c418e93d23ec
Christian Hahn <christian-hahn1@gmx.net>
2020-12-26 15:17:18 +0000

    - added various xconnect specific projects and settings

---
+++ src/Apps/App.XConnect.ModelBuilder/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using Sitecore.XConnect.Schema;
+using Blueprint.Feature.Demo.XConnect.Model;
+
+namespace App.XConnect.ModelBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = GetOutputPath(args);
+
+            // Call SerializeModel for each custom model here
+            SerializeModel(path, DemoModel.Model);
+        }
+
+        private static string GetOutputPath(string[] args)
+        {
+            return args.Length > 0 ? Directory.CreateDirectory(args[0]).FullName : "../../../../environment/xconnect/App_Data/Models";
+        }
+
+        private static void SerializeModel(string path, XdbModel model)
+        {
+            var fileName = model + ".json";
+            var filePath = Path.Combine(path, fileName);
+            var json = Sitecore.XConnect.Serialization.XdbModelWriter.Serialize(model);
+            File.WriteAllText(filePath, json);
+            Console.WriteLine($"Serialized Xdb model {filePath}");
+        }
+    }
+}
