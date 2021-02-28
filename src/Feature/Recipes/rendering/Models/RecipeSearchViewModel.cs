03a2a3c9ba7e889713d182e11f6bd81f1f272cca
Christian Hahn <christian-hahn1@gmx.net>
2021-02-28 10:59:06 +0000

    - added attributes

--- src/Feature/Recipes/rendering/Models/RecipeSearchViewModel.cs
+++ src/Feature/Recipes/rendering/Models/RecipeSearchViewModel.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Sitecore.AspNet.RenderingEngine.Binding.Attributes;
 
 namespace Blueprint.Feature.Recipes.Models
 {
     public class RecipeSearchViewModel
     {
+        [SitecoreComponentField]
         public string Headline { get; set; }
+        [SitecoreComponentField]
         public IEnumerable<RecipeViewModel> Recipes { get; set; }
     }
 }
