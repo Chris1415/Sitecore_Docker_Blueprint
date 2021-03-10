efb3de7b73db97269bd1d5ee4c53c384191c72bb
Christian Hahn <christian-hahn1@gmx.net>
2021-03-10 10:22:15 +0000

    - fixed issue where no feature projects for rendering host were able to add

---
+++ src/Feature/BasicContent/rendering/Extensions/RenderingEngineOptionsExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Blueprint.Feature.BasicContent.Models;
+using Sitecore.AspNet.RenderingEngine.Configuration;
+using Sitecore.AspNet.RenderingEngine.Extensions;
+
+namespace Blueprint.Feature.BasicContent.Extensions
+{
+    public static class RenderingEngineOptionsExtensions
+    {
+        public static RenderingEngineOptions AddFeatureBasicContent(this RenderingEngineOptions options)
+        {
+            options
+                .AddModelBoundView<HeroBanner>("HeroBanner");
+            return options;
+        }
+    }
+}
---
+++ src/Feature/BasicContent/rendering/Models/HeroBanner.cs
@@ -0,0 +1,11 @@
+﻿using Sitecore.LayoutService.Client.Response.Model.Fields;
+
+namespace Blueprint.Feature.BasicContent.Models
+{
+    public class HeroBanner
+    {
+        public TextField Title { get; set; }
+        public TextField Subtitle { get; set; }
+        public ImageField Image { get; set; }
+    }
+}
